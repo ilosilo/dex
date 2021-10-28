a59a97b519cebbbed286ef8afd0709ef9d13e459
ilosilo <islekilayda@gmail.com>
2021-10-28 17:34:58 +0300

    new files

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import configureStore from "./redux/configureStore";
 
+
 const store = configureStore();
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <App/>
     </React.StrictMode>
   </Provider>,
   document.getElementById("root")
